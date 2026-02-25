cec51fe854cdff8a58aa1203b52721d3081e07da
Kaleksandra27 <alekaleale29@gmail.com>
2026-02-25 20:14:00 +0100

    Add AboutUs component for nursery information

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+function AboutUs() {
+  return (
+    <div className="about">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a family-owned online plant shop offering
+        a wide variety of beautiful houseplants. We believe plants
+        bring life and happiness into every home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
